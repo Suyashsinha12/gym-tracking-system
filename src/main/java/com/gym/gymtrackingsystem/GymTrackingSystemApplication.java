79419105ee82c8b7b28eb3bddfe4c19faa678a10
Suyashsinha12 <52148688+Suyashsinha12@users.noreply.github.com>
2026-08-03 23:06:35 +0530

    Add files via upload

---
+++ src/main/java/com/gym/gymtrackingsystem/GymTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gym.gymtrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrackingSystemApplication.class, args);
+	}
+
+}
